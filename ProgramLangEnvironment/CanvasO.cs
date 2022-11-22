77ea7d229562853b3e0962c0e9519570f8bf6cb1
MahatJ <mahat.jalal@gmail.com>
2022-11-22 19:40:39 +0000

    Added fill command,reset command.

--- ProgramLangEnvironment/CanvasO.cs
+++ ProgramLangEnvironment/CanvasO.cs
@@ -11,18 +11,24 @@ namespace ProgramLangEnvironment
     {
         Graphics g;
         Pen Pen;
+       Brush brush = Brushes.Black;
         int posx;
         int posy;
-
+     bool fillo=false;
         public CanvasO(Graphics g)
         {
             this.g = g;
             posx = posy = 0; ;
             Pen = new Pen(Color.Black, 1);
+           
+           
+       
+
         }
 
         public void DrawTo(int x,int y)
         {
+           
             g.DrawLine(Pen, posx, posy, x, y);
             posx = x;
             posy = y;
@@ -38,6 +44,13 @@ namespace ProgramLangEnvironment
         }
 
 
+        public void reset()
+        {
+            posx = 0;
+            posy = 0;
+         
+
+        }
 
         public void clear()
         {
@@ -46,20 +59,43 @@ namespace ProgramLangEnvironment
 
         }
 
+        public void fill(string o)
+        {
+
+            if (o=="on")
+            {
+                fillo = true;
+               
+            }
+           else if (o=="off")
+                { fillo = false; }
+
+
+        }
+
 
         public void DrawRect(int length,int height)
         {
-            g.DrawRectangle(Pen, posx, posy, posx+length, posy+height);
          
-
+          //  g.DrawRectangle(Pen, posx, posy, posx+length, posy+height);
+            Rectangle rect = new Rectangle(posx, posy, length,height);
+          
+          
+                g.DrawRectangle(Pen, rect);
+            if (fillo) { g.FillRectangle(brush, rect); }
+            Console.WriteLine(fillo);
         }
         public void DrawCircle(int radius)
         {
-            g.DrawEllipse(Pen, posx, posy, (posx + (radius * 2)), (posy + (radius * 2)));
+           
+            Rectangle recta = new Rectangle(posx - radius, posy - radius, radius * 2, radius * 2);
+            g.DrawEllipse(Pen, recta);
+            if (fillo) { g.FillEllipse(brush, recta); }
+            //  g.DrawEllipse(Pen, posx, posy, (posx + (radius * 2)), (posy + (radius * 2)));
         }
-        public void DrawTriangle(int x1,int y1,int x2, int y2,int x3, int y3)
+        public void DrawTriangle(int x1,int y1,int x2, int y2,int x3, int y3) // x and y co ords of 3 points on triangle
         {
-            Point[] pnt = new Point[3];
+            Point[] pnt = new Point[3]; //x and y stored in Points array and used with DrawPolygon()
 
             pnt[0].X = x1;
             pnt[0].Y = y1;
--- ProgramLangEnvironment/Form1.cs
+++ ProgramLangEnvironment/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace ProgramLangEnvironment
     {
         const int bmpx = 640;
         const int bmpy = 480;
+
         Bitmap OutBmp = new Bitmap(bmpx, bmpy);
         CanvasO Canvas;
         public Form1()
@@ -41,106 +43,138 @@ namespace ProgramLangEnvironment
             string[] com = line.Split(' ');   //Split into command and parameters
             string command = com[0]; //Assign command to a variable           
             List<int> Param = new List<int>(); // Create list of integers to store parameters after conversion
-            try
+          //  String[] Params = new string[];
+
+            if (com.Length > 1)
             {
-                string Params = com[1]; // Assign chunk of whole parameter to a string to further split
-                string[] ParamL = Params.Split(','); //Split whole parameter string into array of parameter strings
+                string[] Params = com[1].Split(',');
+
+                // Assign chunk of whole parameter to a string to further split
+                //Split whole parameter string into array of parameter strings
 
 
-                foreach (string i in ParamL) // Iterate through each parameter in string form and convert to int
+                foreach (string i in Params) // Iterate through each parameter in string form and convert to int
                 {
                     int a; // Variable to hold integer output
+
                     int.TryParse(i, out a); // TryParse the string as int - surround with try catch
                     Param.Add(a); //Add int to list of parameters
                 }
-            }
-            catch
-            {
+
+         
 
             }
-            finally
+
+            if (command.Equals("drawto"))
             {
-                if (command.Equals("drawto"))
+                if (Param.Count() == 2)
                 {
-                    if (Param.Count() == 2)
+
+
+                    Canvas.DrawTo(Param[0], Param[1]);
+                    //    MessageBox.Show("Params are " + Param[0] + Param[1]);
+
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Parameters : drawTo takes 2 parameters : x and y");
+                }
+            }
+
+
+
+            if (command.Equals("moveto"))
+            {
+                if (Param.Count() == 2)
+                {
+
+
+                    Canvas.MoveTo(Param[0], Param[1]);
+
+                    Debug.WriteLine("x is " + Param[0] + ". y is " + Param[1]);
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Parameters : moveTo takes 2 parameters : x and y");
+                }
+            }
+
+            else if (command.Equals("rect"))
+            {
+                if (Param.Count() == 2)
+                {
+                    Canvas.DrawRect(Param[0], Param[1]);
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Parameters : Rect takes 2 parameters : length and height");
+                }
+
+            }
+            else if (command.Equals("circle"))
+            {
+                if (Param.Count() == 1)
+                {
+                    Canvas.DrawCircle(Param[0]);
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Parameters : Circle takes 1 parameter: radius");
+                }
+            }
+            else if (command.Equals("triangle"))
+            {
+                if (Param.Count() == 6)
+                {
+                    Canvas.DrawTriangle(Param[0], Param[1], Param[2], Param[3], Param[4], Param[5]);
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Parameters : Triangle takes 6 parameters : x1 and y1, x2 and y2, x3 and y3");
+                }
+            }
+            else if (command.Equals("clear"))
+            {
+                Canvas.clear();
+            }
+            else if (command.Equals("reset"))
+            {
+                Canvas.reset();
+            }
+            else if (command.Equals("pen"))
+            {
+                Canvas.reset();
+            }
+           else  if (command.Equals("fill"))
+            {
+                if (com.Length > 1)
+                {
+                    if (com[1] == "on" || com[1] == "off")
                     {
-
-
-                        Canvas.DrawTo(Param[0], Param[1]);
-                        //    MessageBox.Show("Params are " + Param[0] + Param[1]);
-
-
-
+                        Canvas.fill(com[1]);
                     }
                     else
                     {
-                        MessageBox.Show("Invalid Parameters : drawTo takes 2 parameters : x and y");
+                        MessageBox.Show("Invalid Parameters : fill takes 1 parameter: on/off ");
                     }
                 }
-
-
-
-                if (command.Equals("moveto"))
-                {
-                    if (Param.Count() == 2)
-                    {
-
-
-                        Canvas.MoveTo(Param[0], Param[1]);
+            }
 
 
 
 
-                    }
-                    else
-                    {
-                        MessageBox.Show("Invalid Parameters : moveTo takes 2 parameters : x and y");
-                    }
-                }
-
-                else if (command.Equals("rect"))
-                {
-                    if (Param.Count() == 2)
-                    {
-                        Canvas.DrawRect(Param[0], Param[1]);
-                    }
-                    else
-                    {
-                        MessageBox.Show("Invalid Parameters : Rect takes 2 parameters : length and height");
-                    }
-
-                }
-                else if (command.Equals("circle"))
-                {
-                    if (Param.Count() == 1)
-                    {
-                        Canvas.DrawCircle(Param[0]);
-                    }
-                    else
-                    {
-                        MessageBox.Show("Invalid Parameters : Circle takes 1 parameter: radius");
-                    }
-                }
-                else if (command.Equals("triangle"))
-                {
-                    if (Param.Count() == 6)
-                    {
-                        Canvas.DrawTriangle(Param[0], Param[1], Param[2], Param[3], Param[4], Param[5]);
-
-
-                    }
-                    else
-                    {
-                        MessageBox.Show("Invalid Parameters : Triangle takes 6 parameters : x1 and y1, x2 and y2, x3 and y3");
-                    }
-                }
-                else if (command.Equals("clear"))
-                {
-                    Canvas.clear();
-                }
-
+            else
+            {
+                MessageBox.Show("Command not recognised. Valid Commands: rect,triangle,circle,drawTo,moveTo,reset,clear,pen,fill");
             }
         }
+
 
 
 
