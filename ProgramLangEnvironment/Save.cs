faf0b18a385b8fa12433ecb2e8889247c112fcae
MahatJ <mahat.jalal@gmail.com>
2023-02-03 03:02:48 +0000

    Added save command

---
+++ ProgramLangEnvironment/Save.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ProgramLangEnvironment
+{
+    class Save : StoreCmd
+    {
+
+        public Save() : base()
+        { }
+
+
+
+        public void set(Form1 form, string filepath)
+        {
+            this.form = form;
+            this.filePath = filepath;
+        }
+        public override string ToString()
+        {
+            return "Save";
+        }
+        public override int parameters()
+        {
+            return 0;
+        }
+        public override void execute()
+        {
+
+            File.WriteAllText(filePath + ".txt", form.programWindow.Text);
+
+        }
+    }
+}
