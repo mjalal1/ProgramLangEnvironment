2feaedbac2b118a6c2addbdd8fde7513c086c3f6
MahatJ <mahat.jalal@gmail.com>
2023-02-05 22:30:34 +0000

    Variables working now and can be parsed,e.g, var a,a = 10,circle a

--- ProgramLangEnvironment/Parser.cs
+++ ProgramLangEnvironment/Parser.cs
@@ -17,8 +17,8 @@ namespace ProgramLangEnvironment
         public int svprogramCounter;
         public int methodCount;
         public int varCount;
-  
-  
+
+
         public Parser(Form1 form)
         {
             this.form = form;
@@ -37,144 +37,152 @@ namespace ProgramLangEnvironment
             string[] comLine = line.Split('\n'); //Splits on new line, splits program into different lines
             for (int i = 0; i < comLine.Count(); i++)
             {
-  ParseLine(comLine[programCounter]);
-            programCounter++;
+                ParseLine(comLine[programCounter]);
+                programCounter++;
             }
-   
-          
+
+
 
         }
 
 
         private void ParseLine(string comLine)
         {
-           
 
 
-                string[] com = comLine.Split(' ');   //Split into command and parameters
-                string command = com[0]; //Assign command to a variable           
-                List<int> Param = new List<int>(); // Create list of integers to store parameters after conversion
 
-                if (com.Length > 1) // if there are parameters
+            string[] com = comLine.Split(' ');   //Split into command and parameters
+            string command = com[0]; //Assign command to a variable           
+            List<int> Param = new List<int>(); // Create list of integers to store parameters after conversion
+
+            if (com.Length > 1) // if there are parameters
+            {
+                string[] Params = com[1].Split(','); //Split whole parameter into array of parameter strings
+
+                for (int i = 0; i < Params.Count(); i++)
                 {
-                    string[] Params = com[1].Split(','); //Split whole parameter into array of parameter strings
-
-                    foreach (string x in Params) // Iterate through each parameter in string form and convert to int
+                    if (form.varName.Contains(Params[i]))
                     {
-
-                    if (form.varName.Contains(x))
-                    { int found = form.varName.FindIndex(a => a.Contains(x));
-                        x = form.varValue[found].ToString(); }
+                        int found = form.varName.FindIndex(a => a.Contains(Params[i]));
+                        Params[i] = form.varValue[found].ToString();
+                    }
                     //int a; // Variable to hold integer output
                     try
-                        {
-                            int.TryParse(x, out int a);// TryParse the string as int - surround with try catch
-                            Param.Add(a);//Add int to list of parameters
-                        }
-                        catch (Exception)
-                        {
-                            throw new ApplicationException("Invalid Parameters ");
-                        }
+                    {
+                        int.TryParse(Params[i], out int a);// TryParse the string as int - surround with try catch
+                        Param.Add(a);//Add int to list of parameters
+                    }
+                    catch (Exception)
+                    {
+                        throw new ApplicationException("Invalid Parameters ");
                     }
                 }
 
 
-                if (command.Equals("run"))
+                foreach (string x in Params) // Iterate through each parameter in string form and convert to int
                 {
-                    ParseProgram(form.programWindow.Text);// To run pW code, parseCommand is called recursively on the pw.Text
-                    return;
-                }
-                if (com[1].Equals("="))
-                {
-                   
 
-                    DataTable dt = new DataTable();
-                    int v = (int)dt.Compute(com[2], "");
-
-                    ShapeFactory sf2 = new ShapeFactory(); 
-                    dynamic c2 = sf2.GetCmd(com[1]);
-                    c2.set(this.form, command);
-                    c2.setVal(command, v);
-                    return;
-                
 
                 }
+            }
 
-                ShapeFactory sf = new ShapeFactory();  // move to inside for loop ~39, if cmdType=draw do int param, if =declare nest if com[1] is = then set execute, if com[1]
-                dynamic c = sf.GetCmd(command);
+
+            if (command.Equals("run"))
+            {
+                ParseProgram(form.programWindow.Text);// To run pW code, parseCommand is called recursively on the pw.Text
+                return;
+            }
+            if (com[1].Equals("="))
+            {
+
+
+                DataTable dt = new DataTable();
+                int v = (int)dt.Compute(com[2], "");
+
+                ShapeFactory sf2 = new ShapeFactory();
+                dynamic c2 = sf2.GetCmd(com[1]);
+                c2.set(this.form, command);
+                c2.setVal(command, v);
+                return;
+
+
+            }
+
+            ShapeFactory sf = new ShapeFactory();  // move to inside for loop ~39, if cmdType=draw do int param, if =declare nest if com[1] is = then set execute, if com[1]
+            dynamic c = sf.GetCmd(command);
+            if ((Param.Count() != c.parameters()) || (Param.Contains(0))) // ! check for zero doesnt work for string paramets 
+            {
+
+                // throw new ApplicationException("Invalid Parameters : " + c.ToString() + " takes " + c.parameters() + " parameter(s)");
+            }
+
+            if (c.cmdType() == "Draw")
+            {
                 if ((Param.Count() != c.parameters()) || (Param.Contains(0))) // ! check for zero doesnt work for string paramets 
                 {
+                    throw new ApplicationException("Invalid Parameters : " + c.ToString() + " takes " + c.parameters() + " parameter(s)");
+                }
+                c.set(form.Canvas, Param.ToArray());
+                c.execute();
 
-                    // throw new ApplicationException("Invalid Parameters : " + c.ToString() + " takes " + c.parameters() + " parameter(s)");
+            }
+            else if (c.cmdType() == "Store")
+            {
+                if ((Param.Count() != c.parameters()))
+                {
+                    throw new ApplicationException("Invalid Parameters : " + c.ToString() + " takes " + c.parameters() + " parameter(s)");
+                }
+                c.set(form, com[1]);
+                c.execute();
+            }
+            else if (c.cmdType() == "Conditional")
+            {
+                if ((Param.Count() != c.parameters()))
+                {
+                    throw new ApplicationException("Invalid Parameters : " + c.ToString() + " takes " + c.parameters() + " parameter(s)");
+                }
+                c.validate();
+            }
+            else if (c.cmdType() == "Declare")
+            {
+                if ((Param.Count() != c.parameters()))
+                {
+                    throw new ApplicationException("Invalid Parameters : " + c.ToString() + " takes " + c.parameters() + " parameter(s)");
+                }
+                if (com.Length > 2)
+                {
+                    if (com[1] == "=")
+                    {
+                        c.set(this.form, command, com[2]);
+                        c.execute();
+                    }
                 }
 
-                if (c.cmdType() == "Draw")
+                if (command == "call")
                 {
-                    if ((Param.Count() != c.parameters()) || (Param.Contains(0))) // ! check for zero doesnt work for string paramets 
-                    {
-                        throw new ApplicationException("Invalid Parameters : " + c.ToString() + " takes " + c.parameters() + " parameter(s)");
-                    }
-                    c.set(form.Canvas, Param.ToArray());
+                    c.declare(this, com[1]);
                     c.execute();
 
                 }
-                else if (c.cmdType() == "Store")
+                else if (command == "method")
                 {
-                    if ((Param.Count() != c.parameters())) 
-                    {
-                        throw new ApplicationException("Invalid Parameters : " + c.ToString() + " takes " + c.parameters() + " parameter(s)");
-                    }
-                    c.set(form, com[1]);
+                    c.set(this, com[1], programCounter);
                     c.execute();
                 }
-                else if (c.cmdType() == "Conditional")
+                else if (command == "var")
                 {
-                    if ((Param.Count() != c.parameters())) 
-                    {
-                        throw new ApplicationException("Invalid Parameters : " + c.ToString() + " takes " + c.parameters() + " parameter(s)");
-                    }
-                    c.validate();
-                }
-                else if (c.cmdType() == "Declare")
-                {
-                    if ((Param.Count() != c.parameters())) 
-                    {
-                        throw new ApplicationException("Invalid Parameters : " + c.ToString() + " takes " + c.parameters() + " parameter(s)");
-                    }
-                    if (com.Length > 2)
-                    {
-                        if (com[1] == "=")
-                        {
-                            c.set(this.form, command, com[2]);
-                            c.execute();
-                        }
-                    }
-
-                    if (command == "call")
-                    {
-                        c.declare(this, com[1]);
-                        c.execute();
-
-                    }
-                    else if (command == "method")
-                    {
-                        c.set(this, com[1], programCounter);
-                        c.execute();
-                    }
-                    else if (command == "var")
-                    {
-                        c.set(this.form, com[1]);
+                    c.set(this.form, com[1]);
                     c.declare();
-                  //      c.execute();
-                    }
-                    else if (command == "run")
-                    {
-                        c.set(this.form);
-                        c.execute();
-                    }
+                    //      c.execute();
+                }
+                else if (command == "run")
+                {
+                    c.set(this.form);
+                    c.execute();
+                }
 
 
-                 // end of for loop - loops to next line
+                // end of for loop - loops to next line
             }
             //     c.execute();
 
